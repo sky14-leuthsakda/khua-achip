e2fc29a4cf4ff1b21295ce5fd0832c9863b160b1
sukanya <sukanya28988lsdd@gmail.com>
2026-09-19 17:38:37 +0700

    chore: apply saved gitignore and clean node_modules

--- node_modules/@khua-achip/web/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CmvHgJFt.js","_app/immutable/chunks/CupSigZ2.js","_app/immutable/chunks/ucirV5Fr.js","_app/immutable/chunks/CcGkifcF.js","_app/immutable/chunks/WMb57OuR.js","_app/immutable/chunks/B-hmrK5E.js"];
-export const stylesheets = [];
-export const fonts = [];
